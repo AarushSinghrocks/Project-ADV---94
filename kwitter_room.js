0eacb97f40454d57ea171338cd433d788d1f36e9
AarushSinghrocks <87030386+AarushSinghrocks@users.noreply.github.com>
2022-01-26 13:27:59 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDkMKD1Ft-zZ9-gHlsinpJCpLD5MtQlV0w",
+    authDomain: "project---kwitter.firebaseapp.com",
+    projectId: "project---kwitter",
+    storageBucket: "project---kwitter.appspot.com",
+    messagingSenderId: "158709753753",
+    appId: "1:158709753753:web:eaa592bef65d9ce117d508"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
